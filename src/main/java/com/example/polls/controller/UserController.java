8e0905851add9cf25c20e3e09b5d89c43e58243f
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2020-11-10 15:27:11 +0600

    some

---
+++ src/main/java/com/example/polls/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.polls.controller;
+
+import com.example.polls.payload.*;
+import com.example.polls.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+
+
+
+}
